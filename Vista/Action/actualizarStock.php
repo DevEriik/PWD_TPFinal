11dd6e50882c0e78f03164eaa315daa6071942c7
Erick Gonzalez <gempro30yt@gmail.com>
2025-11-27 15:43:35 -0300

    Update actualizarStock.php

--- Vista/Action/actualizarStock.php
+++ Vista/Action/actualizarStock.php
@@ -1,7 +1,6 @@
 <?php
 include_once '../../configuracion.php';
 
-
 $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 $isPostOrGet = $_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['REQUEST_METHOD'] === 'GET';
 $isValidToken = isset($_POST['form_security_token']) && $_POST['form_security_token'] === 'valor_esperado';
